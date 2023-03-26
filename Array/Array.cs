5c8e145b5eced4765a17ee9519107531c214d4d9
emir karatas <emirkaratassss@gmail.com>
2023-03-26 19:15:20 +0300

    Array - ArrayTests

---
+++ Array/Array.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+
+namespace Array
+{
+    public class Array : IEnumerable
+    {
+        private Object[] _InnerArray;
+        private int index = 0;
+        public int Count => index;
+        public int Capacity => _InnerArray.Length;
+        public Array()
+        {
+            _InnerArray = new Object[4];
+        }
+
+        public Array(params Object[] items)
+        {
+            var newArray = new Object[items.Length];
+            System.Array.Copy(items, newArray, items.Length);
+            _InnerArray = newArray;
+            index = items.Count();
+        }
+
+        public void Add(Object item)
+        {
+            if (index==Capacity)
+            {
+                DoubleArray(_InnerArray);
+            }
+            _InnerArray[index] = item;
+            index++;
+        }
+
+        private void DoubleArray(object[] innerArray)
+        {
+            var newArray = new Object[innerArray.Length * 2];
+            System.Array.Copy(innerArray, newArray, innerArray.Length);
+            _InnerArray = newArray;
+        }
+
+        public Object GetItem(int position)
+        {
+            if (position < 0 || position >= _InnerArray.Length) throw new IndexOutOfRangeException();
+            return _InnerArray[position];
+        }
+
+        public void SetItem(int position, Object item)
+        {
+            if (position < 0 || position >= _InnerArray.Length) throw new IndexOutOfRangeException();
+            _InnerArray[position] = item;
+        }
+
+        public int Find(Object item)
+        {
+            for (int i = 0; i < _InnerArray.Length; i++)
+            {
+                if (item.Equals(_InnerArray[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public Object[] Copy(int v1, int v2)
+        {
+            var newArray = new Object[v2];
+            int j = 0;
+
+            for (int i = v1; i < v2; i++)
+            {
+                newArray[j] = GetItem(i);
+                j++;
+            }
+            return newArray;
+        }
+
+        public Object RemoveItem(int position)
+        {
+            var item = GetItem(position);
+            SetItem(position, null);
+
+            for (int i = 0; i < Count-1; i++)
+            {
+                Swap(i, i + 1);
+            }
+            index--;
+
+            if (index == Capacity/2)
+            {
+                var newArray = new Object[_InnerArray.Length / 2];
+                System.Array.Copy(_InnerArray, newArray, newArray.Length);
+                _InnerArray = newArray;
+            }
+            return item;
+        }
+
+        public void Swap(int i, int v)
+        {
+            var temp = _InnerArray[i];
+            _InnerArray[i] = _InnerArray[v];
+            _InnerArray[v] = temp;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _InnerArray.GetEnumerator();
+        }
+    }
+}
---
+++ ArrayTests/ArrayTests.cs
@@ -0,0 +1,155 @@
+namespace ArrayTests
+{
+    public class ArrayTests
+    {
+        [Fact]
+        public void ArrayCountTest()
+        {
+            //Arrange
+            var array = new Array.Array();
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+
+            //Act
+            int count = array.Count;
+
+            //Assertion
+            Assert.Equal(3, count);
+            Assert.Equal(4, array.Capacity);
+        }
+
+        [Fact]
+        public void ArrayGetItemTest()
+        {
+            var array = new Array.Array();
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+
+            var item = array.GetItem(2);
+            
+            Assert.Equal(3, item);
+        }
+
+        [Fact]
+        public void ArrayFindTest()
+        {
+            var array = new Array.Array();
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            
+            int result = array.Find(1);
+            int result2 = array.Find(4);
+
+            Assert.Equal(result, 0);
+            Assert.Equal(result2, -1);
+        }
+
+        [Fact]
+        public void ArrayEnumeratorTest()
+        {
+            var array = new Array.Array();
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+
+            string result = "";
+            foreach (var item in array)
+            {
+                result = string.Concat(result, item);
+            }
+
+            Assert.Equal(result, "123");
+        }
+
+        [Fact]
+        public void ArrayConstructorTest()
+        {
+            var array = new Array.Array(1, 2, 3, 4, 5);
+            array.Add(6);
+            Assert.Equal(6, array.Count);
+        }
+
+        [Fact]
+        public void ArraySetItemTest()
+        {
+            var array = new Array.Array(2,3,4,5);
+            array.SetItem(2, 6);
+            Assert.Equal(6, array.GetItem(2));
+        }
+
+        [Fact]
+        public void ArrayGetItemExceptionTest()
+        {
+            try
+            {
+                var array = new Array.Array();
+                array.Add(1);
+                array.Add(3);
+                array.Add(5);
+
+                var item = array.GetItem(-1);
+
+                Assert.False(true);
+            }
+            catch (Exception)
+            {
+                Assert.True(true);
+            }
+        }
+
+        [Fact]
+        public void ArrayCopyTest()
+        {
+            var array = new Array.Array();
+
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.Add(4);
+
+            var newArray = array.Copy(1, 3);
+            var item = newArray[1];
+
+            Assert.Equal(3, item);
+        }
+
+        [Fact]
+        public void ArraySwapTest()
+        {
+            var array = new Array.Array();
+
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.Add(4);
+
+            array.Swap(0, 2);
+            var item1 = array.GetItem(0);
+            var item2 = array.GetItem(2);
+
+            Assert.Equal(item1, 3);
+            Assert.Equal(item2, 1);
+
+        }
+
+        [Fact]
+        public void ArrayRemoveTest()
+        {
+            var array = new Array.Array();
+
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.Add(4);
+
+            var item = array.RemoveItem(2);
+            var item2 = array.GetItem(2);
+
+            Assert.Equal(3, item);
+            Assert.Equal(item2, 4);
+        }
+    }
+}
